8d11156482c74ba18ad0b2c73eec6d34476a981d
Felipe Lückemeyer <felipe.luckemeyer10@gmail.com>
2020-08-30 23:56:24 -0300

    initial-style

--- front/src/components/Cards/card.js
+++ front/src/components/Cards/card.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import * as s from "./styled-card";
 
-const Card = () => {
+const Card = ({ carta }) => {
   return (
-    <div>
-      Card
-    </div>
+    <s.Container>
+      {/* header */}
+      <div style={{
+        display: "flex",
+        alignItems: "center"
+      }}>
+        <s.Status status="mana" size={23}>
+          <label>{carta.mana}</label>
+        </s.Status>
+        <s.Title>
+          <label>{carta.nome}</label>
+        </s.Title>
+      </div>
+      {/* body */}
+      <div>
+        <s.Hero>
+          <img>
+          </img>
+        </s.Hero>
+        <s.Status status="ataque" size={23}>
+          <label>{carta.ataque}</label>
+        </s.Status>
+        <s.Status status="vida" size={23}>
+          <label>{carta.vida}</label>
+        </s.Status>
+
+      </div>
+
+
+      {/* footer */}
+
+    </s.Container>
   )
 }
 
---
+++ front/src/components/Cards/styled-card.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { Colors } from "../../utils/styled-global"
+
+export const Container = styled.div`
+  width:125px;
+  height: 180px;
+  background: #624C3C;
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+`;
+export const Title = styled.div`
+  display: flex;
+  width:85px;
+  height: 11px;
+  background: #FFFFFF;
+  border-radius: 2px;
+
+  label{
+    font-family: IM FELL DW Pica SC;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 8px;
+    line-height: 10px;
+    color: #000000;
+  }
+`;
+
+export const Hero = styled.div`
+  width:100px;
+  height: 121px;
+  background: #FFFFFF;
+  border: 2px solid ${Colors.begeEscuro};
+
+  box-shadow: inset -2px -2px 2px rgba(0, 0, 0, 0.25), inset 2px 2px 2px rgba(0, 0, 0, 0.25);
+`;
+
+export const Status = styled.div`
+  height:${props => `${props.size}px`};
+  width:${props => `${props.size}px`};
+  border-radius: 50px;
+  display: flex;
+  justify-content:center;
+  align-items:center;
+  background: ${props => {
+    switch (props.status) {
+      case "ataque":
+        return Colors.laranja
+      case "vida":
+        return Colors.vermelho
+      case "mana":
+        return Colors.azulClaro
+      default:
+        break;
+    }
+  }};
+
+  label{
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 21px;
+    color: #FFFFFF;
+  }
+`;
