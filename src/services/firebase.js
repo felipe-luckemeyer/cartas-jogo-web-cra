01675c304446a00674736b454c8c34e72813e3ee
Uéslei Suptitz <ueslei.suptitz@paipe.co>
2020-09-22 22:25:08 -0300

    novo firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCN--UKPKEsMAy79kkn7A8q-HS1lMX4dZA",
-    authDomain: "cartas-jogo.firebaseapp.com",
-    databaseURL: "https://cartas-jogo.firebaseio.com",
-    projectId: "cartas-jogo",
-    storageBucket: "cartas-jogo.appspot.com",
-    messagingSenderId: "553167108138",
-    appId: "1:553167108138:web:8ed0d7b1c74a96ce40ab53",
-    measurementId: "G-M3JCJ9Y403"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  apiKey: "AIzaSyDUGv_4yQU3BxvI3UEghcH0AaDfIF58N5o",
+  authDomain: "mythical-cards.firebaseapp.com",
+  databaseURL: "https://mythical-cards.firebaseio.com",
+  projectId: "mythical-cards",
+  storageBucket: "mythical-cards.appspot.com",
+  messagingSenderId: "309809795846",
+  appId: "1:309809795846:web:ee5a48e83427ea62bb0487",
+  measurementId: "G-QEY3E6EVKV",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
