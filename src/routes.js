f73532397d155cf65fb2f26b00d2f792ab6e1bc2
Felipe Lückemeyer <felipe.luckemeyer10@gmail.com>
2021-01-02 00:05:46 -0300

    remove animate presence routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
 import { Menu, Batalha, Colecao, NotFound } from './pages/';
 import { GlobalStyled } from './utils/styled-global';
 
@@ -8,14 +7,12 @@ const Routes = () => {
   return (
     <>
       <GlobalStyled />
-      <AnimatePresence>
-        <Switch>
-          <Route path="/batalha" exact component={Batalha} />
-          <Route path="/colecao" exact component={Colecao} />
-          <Route path="/" exact component={Menu} />
-          <Route path="*" exact component={NotFound} />
-        </Switch>
-      </AnimatePresence>
+      <Switch>
+        <Route path="/batalha" exact component={Batalha} />
+        <Route path="/colecao" exact component={Colecao} />
+        <Route path="/" exact component={Menu} />
+        <Route path="*" exact component={NotFound} />
+      </Switch>
     </>
   );
 };
