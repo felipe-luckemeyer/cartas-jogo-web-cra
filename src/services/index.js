da5f969784a9726896e438b38bcf571d45bfe52c
Felipe Lückemeyer <felipe.luckemeyer10@gmail.com>
2021-02-01 19:16:13 -0300

    correcao variavel de ambiente

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// const { REACT_APP_PUBLIC_URL } = process.env;
+const { REACT_APP_PUBLIC_URL } = process.env;
+
 const api = axios.create({
-  // baseURL: REACT_APP_PUBLIC_URL,
-  baseURL: 'https://cartas-jogo-beta-server.web.app/',
+  baseURL: REACT_APP_PUBLIC_URL,
 });
 
 export { api };
